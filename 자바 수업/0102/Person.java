d6bd54a7934970d8863e67595453bac15ee31cdf
Dong ki <119654357+wtfwtfs@users.noreply.github.com>
2024-01-02 16:57:15 +0900

    Add files via upload

---
+++ 자바 수업/0102/Person.java
@@ -0,0 +1,9 @@
+import java.io.Serializable;
+
+public class Person implements Serializable{
+
+    public String name;
+    public String tel;
+    public String address;
+    public String birth;
+}
