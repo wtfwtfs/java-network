d5348035755d8f61ca5bab9639542403bae30b48
Dong ki <119654357+wtfwtfs@users.noreply.github.com>
2024-01-04 09:04:32 +0900

    Delete 자바 수업/0102 directory

--- 자바 수업/0102/Person.java
+++
@@ -1,9 +0,0 @@
-import java.io.Serializable;
-
-public class Person implements Serializable{
-
-    public String name;
-    public String tel;
-    public String address;
-    public String birth;
-}
